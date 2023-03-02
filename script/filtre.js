10bafe57427d94d6d0601ae916818f7c39a75f03
SIELINOU N. EIRC R <sielinou1605@gmail.com>
2023-03-02 07:13:13 +0100

    update

--- script/filtre.js
+++ script/filtre.js
@@ -30,32 +30,28 @@ divSelect3 = document.createElement('div');
 divSelect3.className = 'col-2 m-2';
 
 templateSelectIngredients = `
- 
-<li class="nav-item dropdown dropdown-mega position-static">
-<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
-    data-bs-auto-close="outside">Megamenu</a>
-<div class="dropdown-menu shadow">
-    <div class="mega-content px-4">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 col-sm-4 col-md-3 py-4">
-                    <p>Description goes here...</p>
-                </div>
-                <div class="col-12 col-sm-4 col-md-3 py-4">
-                    <p>Description goes here...</p>
-                </div>
-                <div class="col-12 col-sm-4 col-md-3 py-4">
-                    <p>Description goes here...</p>
+    <li class="nav-item dropdown dropdown-mega position-static btn btn-primary">
+        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+            data-bs-auto-close="outside">Ingredients</a>
+        <div class="dropdown-menu shadow">
+            <div class="mega-content px-1">
+                <div class="container-fluid">
+                    <div class="row ">
+                        <div class="col-12 col-sm-4 col-md-3 py-3" id="ingredients1">
+                            <p>Description goes here...</p>
+                        </div>
+                        <div class="col-12 col-sm-4 col-md-3 py-3" id="ingredients2">
+                            <p>Description goes here...</p>
+                        </div>
+                        <div class="col-12 col-sm-4 col-md-3 py-3" id="ingredients3">
+                            <p>Description goes here...</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-</li>
+    </li>
 
-    <select class="form-select btn btn-primary" aria-label="fait une selection" id="ingredients">
-        <option selected>Ingredients</option>
-    </select> 
 
 
 `
@@ -63,26 +59,50 @@ divSelect1.innerHTML = templateSelectIngredients;
 selectContent.appendChild(divSelect1);
 
 templateSelectUstensiles = `
-                                    
-<select class="form-select btn btn-primary" aria-label="fait une selection" id="ustensiles">
-    <option selected>Ustensiles</option>
-</select>
+                     
+ <li class="nav-item dropdown dropdown-mega position-static btn btn-secondary">
+     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+         data-bs-auto-close="outside">Ustensiles</a>
+     <div class="dropdown-menu shadow">
+         <div class="mega-content px-4">
+             <div class="container-fluid">
+                 <div class="row ">
+                     <div class="col-12 col-sm-4 col-md-3 py-3" id="ustensiles">
+                         <p>Description goes here...</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </li>  
 `
 
 divSelect2.innerHTML = templateSelectUstensiles;
 selectContent.appendChild(divSelect2);
 
 templateSelectAppareils = `
-                                 
-<select class="form-select btn btn-primary" aria-label="fait une selection" id="appareils">
-    <option selected>Appareils</option>
-</select>
+                     
+ <li class="nav-item dropdown dropdown-mega position-static btn btn-denger">
+     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+         data-bs-auto-close="outside">Appareils</a>
+     <div class="dropdown-menu shadow">
+         <div class="mega-content px-4">
+             <div class="container-fluid">
+                 <div class="row ">
+                     <div class="col-12 col-sm-4 col-md-3 py-3" id="appareils">
+                         <p>Description goes here...</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </li>
 `
 divSelect3.innerHTML = templateSelectAppareils;
 selectContent.appendChild(divSelect3);
 
 
-
+deuxTiers = 2*recipes.length/3;
 
 // CONSTRUCTION DES CARDS
 for (let i = 0; i < recipes.length; i++) {
@@ -158,17 +178,52 @@ for (let i = 0; i < recipes.length; i++) {
 
         
         // INGREDIENTS
-        ingredients = document.getElementById('ingredients');
-        optionI = document.createElement('option');
-        optionI.setAttribute('value', j)
+//         ingredients = document.getElementById('ingredients');
+//         optionI = document.createElement('option');
+//         optionI.setAttribute('value', j)
 
-        let templateOptionI = `
+//         let templateOptionI = `
                         
-        ${recipes[i].ingredients[j].ingredient}
+//         ${recipes[i].ingredients[j].ingredient}
         
-`
-        optionI.innerHTML = templateOptionI;
-        ingredients.appendChild(optionI);
+// `
+//         optionI.innerHTML = templateOptionI;
+//         ingredients.appendChild(optionI);
+ 
+if (i <= (recipes.length/3)) {
+    ingredients = document.getElementById('ingredients1');
+    pI = document.createElement('p');
+    
+    let templateOptionI = `
+                    
+    ${recipes[i].ingredients[j].ingredient}
+    
+    `
+    pI.innerHTML = templateOptionI;
+    ingredients.appendChild(pI);
+} else if ((i > (recipes.length/3)) && (i < deuxTiers)){
+    ingredients = document.getElementById('ingredients2');
+    pI = document.createElement('p');
+    
+    let templateOptionI = `
+                    
+    ${recipes[i].ingredients[j].ingredient}
+    
+    `
+    pI.innerHTML = templateOptionI;
+    ingredients.appendChild(pI);
+}else {
+    ingredients = document.getElementById('ingredients3');
+    pI = document.createElement('p');
+    
+    let templateOptionI = `
+                    
+    ${recipes[i].ingredients[j].ingredient}
+    
+    `
+    pI.innerHTML = templateOptionI;
+    ingredients.appendChild(pI);
+}
 
         
         // APPAREILS
