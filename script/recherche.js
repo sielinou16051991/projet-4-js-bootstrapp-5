2a994c0b20c316f09568f32214b75a7894c328d7
SIELINOU NOUBISSIE ERIC ROMUALD <sielinou1605@gmail.com>
2023-03-07 12:48:17 +0100

    update filters and articles

--- script/recherche.js
+++ script/recherche.js
@@ -1,3 +1,9 @@
+import { recipesJson } from "../data/recipes.js";
+import { searchParams } from "./index.js";
+
+const recipes = JSON.parse(recipesJson);
+
 export function updateResults(){
-    
+    const result = search(searchParams)
+    displayResults(result);
 }
